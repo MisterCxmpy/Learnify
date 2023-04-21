23957d7d956fed84f66cb78b4a6dccf923579552
MisterCxmpy <cosgunhussein@gmail.com>
2023-04-21 19:47:08 +0100

    optimized dashboard page

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -8,83 +8,56 @@ export default function Dashboard() {
   const [favourites, setFavourites] = useState([]);
   const [userCreated, setUserCreated] = useState([]);
 
-  const getUser = async () => {
-    const response = await fetch(
-      `http://localhost:8080/users/username/${localStorage.getItem("user_id")}`
-    );
-
-    setUsername(await response.text());
-  };
-
-  const getFavoritedCards = async () => {
-    const response = await fetch(
-      `http://localhost:8080/flashcards/favorite/user/${localStorage.getItem(
-        "user_id"
-      )}`
-    );
-
-    const data = await response.json();
-
-    setFlashcards(data);
-  };
-
-  const checkFavorites = async () => {
-    const userId = localStorage.getItem("user_id");
-    const response = await fetch(
-      `http://localhost:8080/flashcards/favorite/user/${userId}/`
-    );
-
-    const data = await response.json();
-
-    if (Array.isArray(data) && data.length > 0) {
-      const cardIds = data.map((d) => d.card_id);
-      setFavourites(cardIds);
-    }
-  };
-
-  const getUserCreated = async (e) => {
-    const userId = localStorage.getItem("user_id");
-
-    const response = await fetch(
-      `http://localhost:8080/flashcards/user/${userId}`
-    );
-
-    const data = await response.json();
-
-    if (response.ok) {
-      setUserCreated(data);
-    } else {
-      console.log("Something failed, very sad! :(");
-    }
-  };
-
   useEffect(() => {
-    getUser();
-    checkFavorites();
-    getUserCreated();
+    const fetchUser = async () => {
+      const response = await fetch(
+        `http://localhost:8080/users/username/${localStorage.getItem("user_id")}`
+      );
+      const data = await response.text();
+      setUsername(data);
+    };
+    
+    const fetchFavourites = async () => {
+      const response = await fetch(
+        `http://localhost:8080/flashcards/favorite/user/${localStorage.getItem("user_id")}`
+      );
+      const data = await response.json();
+      setFlashcards(data);
+      setFavourites(data.map((d) => d.card_id));
+    };
+    
+    const fetchUserCreated = async () => {
+      const response = await fetch(
+        `http://localhost:8080/flashcards/user/${localStorage.getItem("user_id")}`
+      );
+      const data = await response.json();
+      if (response.ok) {
+        setUserCreated(data);
+      } else {
+        console.log("Something failed, very sad! :(");
+      }
+    };
+    
+    fetchUser();
+    fetchFavourites();
+    fetchUserCreated();
   }, []);
 
-  useEffect(() => {
-    getFavoritedCards();
-  }, [favourites]);
-
   return (
-    <div className={styles["dashboard"]}>
-      <div className={styles["container"]}>
-        <h1 className={styles["title"]}>Welcome {username}</h1>
-        <div className={styles["content"]}>
+    <div className={styles.dashboard}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Welcome {username}</h1>
+        <div className={styles.content}>
           <h1 className={styles["content-heading"]}>Favourited flashcards</h1>
-          <div className={styles["cards"]}>
-            {Array.isArray(flashcards) && flashcards.length > 0 ? (
-              flashcards.map((f) => {
-                return (
-                  <Flashcard
-                    key={f.card_id}
-                    f={f}
-                    favourites={favourites}
-                  />
-                );
-              })
+          <div className={styles.cards}>
+            {flashcards.length > 0 ? (
+              flashcards.map((flashcard) => (
+                <Flashcard
+                  key={flashcard.card_id}
+                  f={flashcard}
+                  favourites={favourites}
+                />
+              ))
             ) : (
               <p className={styles["no-flash"]}>
                 No favourited flashcards yet...
@@ -92,21 +65,15 @@ export default function Dashboard() {
             )}
           </div>
           <h1 className={styles["content-heading"]}>{username}'s flashcards</h1>
-          <div className={styles["cards"]}>
-            {Array.isArray(userCreated) && userCreated.length > 0 ? (
-              userCreated.map((f) => {
-                return (
-                  <Flashcard
-                    key={f.card_id}
-                    f={f}
-                    getColours={getColours}
-                    handleFlip={handleFlip}
-                    handleFavorites={handleFavorites}
-                    flippedCards={flippedCards}
-                    favourites={favourites}
-                  />
-                );
-              })
+          <div className={styles.cards}>
+            {userCreated.length > 0 ? (
+              userCreated.map((flashcard) => (
+                <Flashcard
+                  key={flashcard.card_id}
+                  f={flashcard}
+                  favourites={favourites}
+                />
+              ))
             ) : (
               <p className={styles["no-flash"]}>No created flashcards yet...</p>
             )}
